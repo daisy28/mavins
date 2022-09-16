10e3de3ff3952c0142713bc400c257d9cd464254
Gloria <ubahgloria16@gmail.com>
2022-09-16 01:43:01 +0100

    Mavins Records landing page styled with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+  "./index.html"],
+  theme: {
+    extend: {
+      colors : {
+        darkbrown: "#4D2A15",
+        brown: "#7B4F35",
+        background: "#D5C8BB",
+        background_two: "#B78A6C",
+        black1: "##131415"
+      },
+
+      boxShadow: {
+        '3xl': '-5px 5px 0 1px rgba(#4D2A15)',
+      }
+    },
+    fontFamily :{
+      'signika': ['Signika Negative', 'sans-serif']
+    } 
+  },
+  plugins: [],
+}
